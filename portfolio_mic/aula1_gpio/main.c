237c882cb1c26674f5b5017e28fe88db83558c70
Adan Kvitschal <adan.kvitschal@ifsc.edu.br>
2026-08-20 11:15:03 -0300

    Código "pisca led" com máscaras binárias

--- portfolio_mic/aula1_gpio/main.c
+++ portfolio_mic/aula1_gpio/main.c
@@ -5,10 +5,16 @@
  *  Author: Adan Kvitschal
  */ 
 
+#define F_CPU 16000000
 #include <xc.h>
+#include "util/delay.h"
 
 int main(void) {
+	DDRB |= 0b00100000; //Define pino PB5 como saída
     while(1) {
-        //TODO:: Please write your application code 
+		PORTB |= 0b00100000; //Atribui nível lógico alto ao pino PB5
+		_delay_ms(100);
+        PORTB &= 0b11011111; //Atribui nível lógico baixo ao pino PB5
+		_delay_ms(900);
     }
 }
