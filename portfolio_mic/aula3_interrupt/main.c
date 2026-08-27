94afd1b6042e2cb4a9bb4a1e0b4f3c3d72cc9328
Adan Kvitschal <adan.kvitschal@ifsc.edu.br>
2026-08-27 11:09:30 -0300

    Introdução às interrupções externas Código com evento de disparo de um LED quando ocorre borda de descida no pino INT0 Incluso arquivo de simulção do SimulIDE v1.0
    Signed-off-by: Adan Kvitschal <adan.kvitschal@ifsc.edu.br>

---
+++ portfolio_mic/aula3_interrupt/main.c
@@ -0,0 +1,39 @@
+/*
+ * main.c
+ *
+ * Created: 8/27/2026 10:25:11 AM
+ *  Author: Adan Kvitschal
+ */ 
+
+#define F_CPU 16000000
+#include <xc.h>
+#include "avr/interrupt.h"
+#include "util/delay.h"
+
+//Configuração da Interrupção Externa (INT0/INT1)
+
+ISR(INT0_vect) { //Função no vetor de interrupção INT0
+	PORTB |= (1<<PORTB1);//Acende led em PB1
+	_delay_ms(100);
+	PORTB &= ~(1<<PORTB1);//Apaga led em PB1
+}
+
+void INT_config() {
+	EICRA = (1<<ISC01)|(0<<ISC00); //configura INT0 na borda de descida
+	EIMSK = (1<<INT0); //Habilita INT0
+}
+
+void GPIO_config() {
+	DDRB |= (1<<DDB0);	// Pino PB0 como saída
+	DDRB |= (1<<DDB1);	// Pino PB1 como saída
+}
+
+int main(void) {
+	GPIO_config();
+	INT_config();
+	sei(); // Habilita Interrupções globalmente
+    while(1) {
+		_delay_ms(100);
+        PORTB ^= (1<<PORTB0); //alterna pino PB0
+    }
+}
