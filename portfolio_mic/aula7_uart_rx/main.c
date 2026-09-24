7ca000142e5eb9d9310e049735dbe2dd118ed13e
Adan Kvitschal <adan.kvitschal@ifsc.edu.br>
2026-09-24 11:04:34 -0300

    Código de recepção UART por bytes individuais O código pisca leds de acordo com o caractere recebido Inclui simulação com transmissor da aula6 ligado ao receptor no simulide

---
+++ portfolio_mic/aula7_uart_rx/main.c
@@ -0,0 +1,45 @@
+/*
+ * main.c
+ *
+ * Created: 9/24/2026 10:20:38 AM
+ *  Author: Adan Kvitschal
+ */ 
+
+#define F_CPU 16000000
+#include <xc.h>
+#include "util/delay.h"
+
+/**
+ * Configuração do módulo USART0 para modo assíncrono, frame de 8 bits, paridade par
+**/
+void UART_config_rx(uint16_t pBAUD) {
+	UCSR0A = (0<<U2X0);							//Modo "double speed" desativado
+	UCSR0B = (1<<RXEN0)|(0<<TXEN0)|(0<<UCSZ02); // Habilita o receptor UART0, frame de 8 bits
+	UCSR0C = (0<<UMSEL01)|(0<<UMSEL00)			// Modo assíncrono
+		   | (1<<UPM01)|(0<<UPM00)				// Habilita paridade par
+		   | (1<<USBS0)							// 2 bits de stop
+		   | (1<<UCSZ01)|(1<<UCSZ00)			// Frame de 8 bits
+		   | (0<<UCPOL0);						// Polaridade do clock: ignorada
+	UBRR0 = (1000000 / pBAUD) - 1;
+}
+
+void GPIO_config() {
+	DDRC = (1<<DDC0)|(1<<DDC1); // PC0 e PC1 como saída
+}
+
+int main(void) {
+	GPIO_config();
+	UART_config_rx(9600);
+    while(1) {
+        while((UCSR0A & (1<<RXC0)) == 0); //Semáforo na flag de recebimento da UART
+		uint8_t tReceivedByte = UDR0;	//Leitura do buffer UART
+		//Tratamento da mensagem recebida
+		if(tReceivedByte == 'M') {
+			PORTC |= (1<<PORTC0); //Led Verde
+		} else {
+			PORTC |= (1<<PORTC1); //Led Vermelho
+		}
+		_delay_ms(1);
+		PORTC = 0; //Apaga todos os LEDs
+    }
+}
